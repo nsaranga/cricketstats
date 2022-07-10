1a77a4ad65d2a3bbe051a82deb1f98f1cf70de0c
nsaranga <nsaranga@gmail.com>
2022-07-10 16:25:28 +1000

    add simulation example file

---
+++ simulationexample.py
@@ -0,0 +1,74 @@
+""" 
+cricketstats is a script for getting team and player statistics from the cricsheet.org database for data analysis.
+Copyright (C) 2021  Saranga Sudarshan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+import cricketstats
+import matchsim
+
+# This is an example script of how you can use cricketstats
+# If you want to import cricketstats into your own python file or jupyter notebook that sits outside, use the lines below instead of "import cricketstats". Replace "./cricketstats/" with the path of repo in your system.
+"""
+import os
+import sys
+module_path = os.path.abspath(os.path.join("./cricketstats/"))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+from cricketstats import cricketstats
+from cricketstats import matchsim
+"""
+
+
+""" 1. Create a simulation object """
+sim1 = matchsim.matchsim(simteams=["Sri Lanka", "Australia"])
+
+""" 2. Apply the "sim()" method to the simulation object with the necessary arguments. """
+# Arguments are mostly lists, with items separated by commas.
+
+# First we can setup our arguments.
+# Required input arguments:
+
+statsdatabase="/home/saranga/Downloads/all_json.zip" # Path of cricsheet.org's all matches json zip file on your system. You can download the file at https://cricsheet.org/downloads/
+statsfrom_date=(2021,7,1) # The (Year, Month, Day) from which to start search for probability values
+statsto_date=(2022,12,31) # The (Year, Month, Day) from which to end search for probability values
+statssex="male" # The male or female team stats to search for
+statsmatchtype = "T20" # Options: ["Test","ODI", "ODM", "T20",] Explanation of what these mean is found at https://cricsheet.org/downloads/
+simulations=10 # This is the number of simulations to run
+
+# Optional Arguments:
+
+inningsorder = None # Eg. ["Sri Lanka", "Australia","Sri Lanka", "Australia"] for a test match # This overrides the toss rng and sets a specific innings order.
+rain=False # This sets whether there might be rain affected matches in the simulated games.
+
+# Apply sim() method on the simulation object.
+sim1.sim(statsdatabase=statsdatabase, statsfrom_date=statsfrom_date,statsto_date=statsto_date, statssex=statssex,statsmatchtype=statsmatchtype,simulations=simulations, inningsorder=inningsorder, rain=rain)
+
+
+# You can use the above template if you want or put all the values inside the brackets of the "sim()" method like I've done below for the object.
+# sim1.sim(statsdatabase="/home/saranga/Downloads/all_json.zip", statsfrom_date=(2021,7,1),statsto_date=(2022,12,31), statssex="male",statsmatchtype="Test",simulations=10)
+
+
+""" 3. Print result. Output is a pandas dataframe. """
+# The output of the search is a pandas dataframe.
+
+print(sim1.results)
+
+# If you want to save the sim result as a csv file that you can open in excel you can use the line below.
+# sim1.results.to_csv(./YOUR FILE LOCATION)

3497e54cde5cd9216fb3a2374673ce7141b59721
mane <nsaranga@hotmail.com>
2022-07-17 17:39:29 +1000

    changed defaults updated example

--- simulationexample.py
+++ simulationexample.py
@@ -21,7 +21,7 @@ import numpy as np
 import pandas as pd
 
 import cricketstats
-import matchsim
+import mpmatchsim
 
 # This is an example script of how you can use cricketstats
 # If you want to import cricketstats into your own python file or jupyter notebook that sits outside, use the lines below instead of "import cricketstats". Replace "./cricketstats/" with the path of repo in your system.
@@ -37,7 +37,7 @@ from cricketstats import matchsim
 
 
 """ 1. Create a simulation object """
-sim1 = matchsim.matchsim(simteams=["Sri Lanka", "Australia"])
+sim = mpmatchsim.matchsim(simteams=["Sri Lanka", "Australia"])
 
 """ 2. Apply the "sim()" method to the simulation object with the necessary arguments. """
 # Arguments are mostly lists, with items separated by commas.
@@ -49,16 +49,17 @@ statsdatabase="/home/saranga/Downloads/all_json.zip" # Path of cricsheet.org's a
 statsfrom_date=(2021,7,1) # The (Year, Month, Day) from which to start search for probability values
 statsto_date=(2022,12,31) # The (Year, Month, Day) from which to end search for probability values
 statssex="male" # The male or female team stats to search for
-statsmatchtype = "T20" # Options: ["Test","ODI", "ODM", "T20",] Explanation of what these mean is found at https://cricsheet.org/downloads/
-simulations=10 # This is the number of simulations to run
+statsmatchtype = "T20" # match type to simulated and search stats for Options: ["Test","ODI", "ODM", "T20",] Explanation of what these mean is found at https://cricsheet.org/downloads/
+simulations=10 # Number of matches to simulate. Make sure this is divisble by the number of cores you have in your computer otherwise the module will fail.
 
 # Optional Arguments:
+inningsorder = None # This sets the order in which teams bat. Eg. ["Sri Lanka", "Australia","Sri Lanka", "Australia"] for a test match # This overrides the toss rng and sets a specific innings order.
+rain=False # This sets whether there might be rain affected matches in the simulated games. This is set at constant 90/10% in favour of no rain.
 
-inningsorder = None # Eg. ["Sri Lanka", "Australia","Sri Lanka", "Australia"] for a test match # This overrides the toss rng and sets a specific innings order.
-rain=False # This sets whether there might be rain affected matches in the simulated games.
+matchscore = None # This sets a score or match situation from which a matches will simulated eg. matchscore = {"Innings 1":["Sri Lanka",10, 300, 90],"Innings 2":["Australia"",10, 300, 90],"Innings 3":["Sri Lanka",10, 300, 90],"Innings 4":["Australia"",0, 1, 1]}. when using this argument,s inningsorder must be given to ensure the correct inningsorder is preserved
 
 # Apply sim() method on the simulation object.
-sim1.sim(statsdatabase=statsdatabase, statsfrom_date=statsfrom_date,statsto_date=statsto_date, statssex=statssex,statsmatchtype=statsmatchtype,simulations=simulations, inningsorder=inningsorder, rain=rain)
+sim.sim(statsdatabase=statsdatabase, statsfrom_date=statsfrom_date,statsto_date=statsto_date, statssex=statssex,statsmatchtype=statsmatchtype,simulations=simulations, inningsorder=inningsorder, rain=rain)
 
 
 # You can use the above template if you want or put all the values inside the brackets of the "sim()" method like I've done below for the object.
@@ -68,7 +69,10 @@ sim1.sim(statsdatabase=statsdatabase, statsfrom_date=statsfrom_date,statsto_date
 """ 3. Print result. Output is a pandas dataframe. """
 # The output of the search is a pandas dataframe.
 
-print(sim1.results)
+print(sim.results)
+
+# You can then use all the familiar pandas methods for statistical analysis and plotting.
+# Eg. sim.simresults["Winner"].value_counts(normalize=True) # This command will given you winnings percentage for each team.
 
 # If you want to save the sim result as a csv file that you can open in excel you can use the line below.
 # sim1.results.to_csv(./YOUR FILE LOCATION)
